3df0fd3a737e3cd54df8a4bcb32ae3370ff8b978
NASINE DE PREZ Eduardo Ezequiel <e2ndp@gmail.com>
2022-08-06 18:52:21 -0300

    Con Templates

--- familia/familia/urls.py
+++ familia/familia/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from familia.views import saludo
+from familia.views import saludo, plantilla
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludo/', saludo, name='saludo'),
+    path('plantilla/', plantilla, name='plantilla'),
 ]
--- familia/familia/views.py
+++ familia/familia/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
 
+from django.shortcuts import render
+
 def saludo(request):
     return HttpResponse("Hola Django-Coder")
+
+def plantilla(request):
+    return render(request, 'plantilla.html', context={})
